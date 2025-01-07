eb24278ce6e26cab48572907991ffda595b3c862
hssong1221 <hssong1221@naver.com>
2025-01-07 22:15:49 +0900

    캐릭터 움직이는거 테스트

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     public float moveSpeed = 1f;
-
+    private Rigidbody2D rb;
+    bool isFront = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position += GameManager.Instance.CalcGameSpeed() * Time.deltaTime * Vector3.left;
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        //float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0).normalized * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
     }
 }
