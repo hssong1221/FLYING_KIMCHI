3e99818199a3db9bed4497b2140f2b24ad46bf21
hssong1221 <hssong1221@naver.com>
2025-01-11 20:48:09 +0900

    양쪽에서 스폰해서 다가오게 만들었음

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -2,16 +2,10 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < -15)
+        if(transform.position.x < -20 || transform.position.x > 20)
         {
             Destroy(gameObject);
         }
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 public class Mover : MonoBehaviour
 {
+    [SerializeField]
+    private bool isReverse = false;
+
     public float moveSpeed = 1f;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += GameManager.Instance.CalcGameSpeed() * Time.deltaTime * Vector3.left;
+        if(!isReverse)
+            transform.position += GameManager.Instance.CalcGameSpeed() * Time.deltaTime * Vector3.left;
+        else
+            transform.position += GameManager.Instance.CalcGameSpeed() * Time.deltaTime * Vector3.right;
     }
 }
