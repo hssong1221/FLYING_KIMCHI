fe9551ef093824b0a9424b4a56d1c96e49d9b858
EBSiNoah <kyb0809tg@naver.com>
2025-01-22 17:32:33 +0900

    Update scene, Add SlowAura gameobject, update player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,11 +7,14 @@ public class Player : MonoBehaviour
     private Animator animator;
     private BoxCollider2D collider2D;
     private SpriteRenderer spriteRenderer;
+    private Coroutine slowAuraCorutine;
 
     public BoxCollider2D rightCollider;
     public BoxCollider2D leftCollider;
+    public GameObject slowAura;
 
     public float moveSpeed = 0f;
+    public float slowAuraDuration = 5f;
     bool isFront = true;
 
     private bool isInvincible = true;
@@ -50,7 +54,28 @@ public class Player : MonoBehaviour
         if (collision.gameObject.tag.Equals("Food"))
         {
             Destroy(collision.gameObject);
-            Heal();
+            var foodItem = collision.gameObject.GetComponent<Item>();
+            if(foodItem.GetAttribute()==0)
+            {
+                Heal();
+            }
+            else if (foodItem.GetAttribute() == 1)
+            {
+                Debug.Log("slow");
+                if(slowAuraCorutine == null)
+                {
+                    slowAuraCorutine = StartCoroutine(ActiveSlowAura());
+                }
+                else
+                {
+                    StopCoroutine(ActiveSlowAura());
+                    slowAuraCorutine = StartCoroutine(ActiveSlowAura());
+                }
+            }
+            else if (foodItem.GetAttribute() == 2)
+            {
+                Debug.Log("shuriken");
+            }
         }
         else if (collision.gameObject.tag.Equals("Gold"))
         {
@@ -100,6 +125,25 @@ public class Player : MonoBehaviour
     {
         collider2D.enabled = false;
         animator.enabled = false;
+    }
+
+    private IEnumerator ActiveSlowAura()
+    {
+        var slowAuraComponent = slowAura.GetComponent<SlowAura>();
+
+        if (slowAura != null)
+        {
+            slowAuraComponent.SwitchSlowAura();
+        }
+
+        yield return new WaitForSeconds(slowAuraDuration);
+        
+        if (slowAura != null) 
+        {
+            slowAuraComponent.SwitchSlowAura();
+        }
+
+        slowAuraCorutine = null;
     }
 }
 
---
+++ Assets/Scripts/SlowAura.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class SlowAura : MonoBehaviour
+{
+    public Transform target;
+    private Vector2 initialOffset;
+    [SerializeField]
+    private bool isActive;
+
+    private void Start()
+    {
+        initialOffset = (Vector2)transform.position - (Vector2)target.position;
+        isActive = false;
+    }
+
+    private void Update()
+    {
+        // 타겟(플레이어)의 현재 위치에 초기 오프셋을 더해 새로운 위치 계산
+        Vector2 newPosition = (Vector2)target.position + initialOffset;
+        // 계산된 새 위치로 이동 - 계속 같은 거리 유지 하면서 따라 다님
+        transform.position = newPosition;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isActive && collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy slow");
+        }
+    }
+
+    public void SwitchSlowAura()
+    {
+        isActive = !isActive;
+    }
+}
